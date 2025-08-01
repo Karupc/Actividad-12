ba0c74304431a25574ee290aa55fe37f1ba8bb17
Karup <kperca802@gmail.com>
2025-08-01 09:48:12 -0600

    Ingreso de notas

--- Actividad 12.py
+++ Actividad 12.py
@@ -1,1 +1,26 @@
+notas = []
 print("---PROGRAMA PARA CALCULAR PROMEDIOS---\n")
+try:
+    cantidad_estudiantes = int(input("¿Cuántos estudiantes desea ingresar?: "))
+except ValueError:
+    print("Error: Debe ingresar un valor numérico")
+except Exception as e:
+    print("Se produjo un error inesperado:", e)
+else:
+    for i in range(cantidad_estudiantes):
+        print(f"Información estudiante #{i+1}")
+        try:
+            nombre_est = input("Ingrese el nombre del estudiante: ")
+            cantidad_notas = int(input("¿Cuántas notas desea ingresar?: "))
+        except ValueError:
+            print("Error: Debe ingresar un valor numérico")
+        except Exception as e:
+            print("Se produjo un error inesperado:", e)
+        else:
+            for i in range(cantidad_notas):
+                try:
+                    nota = input(f"Ingrese nota {i + 1}: ")
+                except ValueError:
+                    print("Error: Debe ingresar un valor numérico")
+                except Exception as e:
+                    print("Se produjo un error inesperado:", e)
